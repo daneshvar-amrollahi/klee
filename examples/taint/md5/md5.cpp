fb1e1ec2cfc0c5df9db82fac6e7e8cf6c9e9f8c2
Solal Pirelli <solal.pirelli@gmail.com>
2021-12-09 10:09:01 +0000

    Simplify md5

--- examples/taint/md5/md5.cpp
+++ examples/taint/md5/md5.cpp
@@ -3,19 +3,27 @@
  *  @brief  Harness for MD5 leakage analysis
  */
 
-#include "md5.h"
-
+#include <stdlib.h>
+#include <string.h>
 #include <cassert>
+
+#include <openssl/conf.h>
+#include <openssl/evp.h>
+#include <openssl/rand.h>
+#include <openssl/err.h>
+#include <openssl/md5.h>
 
 #include <klee/klee.h>
 
-int MD5_digest(unsigned char* in, size_t len, unsigned char out[MD5_DIGEST_LENGTH])
+constexpr size_t DataSize = 64;
+
+static int MD5_digest(unsigned char* in, size_t len, unsigned char out[MD5_DIGEST_LENGTH])
 {
    unsigned int digest_length;
 
    EVP_MD_CTX* ctx = EVP_MD_CTX_new();
    EVP_DigestInit_ex(ctx, EVP_md5(), nullptr);
-   
+
    if (!EVP_DigestUpdate(ctx, in, len)) {
      return -1;
    }
@@ -32,17 +40,17 @@ int MD5_digest(unsigned char* in, size_t len, unsigned char out[MD5_DIGEST_LENGT
 int main(int argc, char** argv)
 {
    // Allocate data
-   unsigned char* data = new unsigned char[Md5DataSize];
+   unsigned char* data = new unsigned char[DataSize];
    unsigned char digest[MD5_DIGEST_LENGTH];
 
    // Make symbolic data
-   klee_make_symbolic(data, Md5DataSize, "data");
+   klee_make_symbolic(data, DataSize, "data");
 
    // Taint data
-   klee_set_taint(1, data, Md5DataSize);
+   klee_set_taint(1, data, DataSize);
 
    // Digest
-   assert(MD5_digest(data, Md5DataSize, digest) > 0);
+   assert(MD5_digest(data, DataSize, digest) > 0);
 
    return 0;
 }
