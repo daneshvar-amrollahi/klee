f891f30ba6f5e7a3ac087b21663eb3c99185b2e4
daneshvar <daneshvar@solo2.maas>
2022-07-22 14:26:33 +0000

    Modified memcmp.c

--- examples/memcmp/memcmp.c
+++ examples/memcmp/memcmp.c
@@ -1,34 +1,38 @@
 #include "klee/klee.h"
+#include <stdio.h>
 
 int match(int *a, int* b, int n) 
 {
-        int i = 0;
-        while (a[i] == b[i] && i < n)
-	{
-		klee_daneshvar();
-                i++;
-	}
+        int i;
+        klee_make_symbolic(&i, sizeof(i), "memcmp_return_value");
+        int forall_quantified_var, exists_quantified_var;
+        klee_make_symbolic(&forall_quantified_var, sizeof(forall_quantified_var), "fqv");
+        klee_make_symbolic(&exists_quantified_var, sizeof(exists_quantified_var), "eqv");
 
-return i;
+        klee_memcmp(forall_quantified_var, exists_quantified_var, a, b, n, i);
+        return i;
 }
 
-int main() {
-        int n;
-        klee_make_symbolic(&n, sizeof (n), "n");
-        klee_assume(n >= 0);
-        klee_assume(n <= 5);
 
-        for (int i = 0; i <= 5; i++){
-                if(i == n){
-                        n = i;
-                        break;
-                }
-        }
+
+// int match(int *a, int* b, int n) 
+// {
+//         int i = 0;
+//         while (i < n && a[i] == b[i])
+//                 i++;
+//         if (i == n)
+//                 return 0;
+//         return 1;
+// }
+
+
+
+int main() {
         int a[10];
         klee_make_symbolic(a, sizeof (a), "a");
 
         int b[10];
         klee_make_symbolic(b, sizeof (b), "b");
 
-        return match(a, b, n);
+        match(a, b, 10);
 }
