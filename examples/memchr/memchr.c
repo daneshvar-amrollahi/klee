486f74d45c4e452e160b7f1e25d7695602344112
Daneshvar Amrollahi <amrollahi.daneshvar@gmail.com>
2022-08-18 15:52:08 +0000

    Cleaned up memchr.c

--- examples/memchr/memchr.c
+++ examples/memchr/memchr.c
@@ -1,24 +1,29 @@
 #include "klee/klee.h"
 #include <stdio.h>
 
+
+/*
+match(char* str, char c, uint32_t n) searches for the first occurrence of the character c in the first n bytes of the string pointed to, by the argument str.
+*/
+
 char* match(char* str, char c, uint32_t n) {
         uint32_t ret, impliesVar;
         klee_make_symbolic(&ret, sizeof(ret), "memchr_return_value");
         klee_make_symbolic(&impliesVar, sizeof(impliesVar), "implies_var");
-        klee_make_symbolic(str, n, "str");
         klee_memchr(str, c, n, ret, impliesVar);
         return str + ret;
 }
 
 int main() {
         char str[5];
-        char c;
-        klee_make_symbolic(&c, sizeof(c), "c");
         uint32_t n = 5;
+        char c;
+        klee_make_symbolic(str, n, "str");
+        klee_make_symbolic(&c, sizeof(c), "c");
         char* res = match(str, c, n);
         if (res == str + n)
         {
-                // klee_assume(str[0] == c); should fail on this, as expected
+                /* klee_assume(str[0] == c); // should fail on this, as expected */
                 printf("match not found\n");
         }
         else
@@ -27,7 +32,7 @@ int main() {
                 klee_assume(str[1] != c);
                 klee_assume(str[2] != c);
                 klee_assume(str[3] != c);
-                klee_assume(str[4] != c); should fail on this, as expected */
+                klee_assume(str[4] != c); // should fail on this, as expected */
                 printf("match found\n");
         }
         
