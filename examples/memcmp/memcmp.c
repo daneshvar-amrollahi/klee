31783020b620ce6f1857a016f6cf5aedebfdc4dd
Daneshvar Amrollahi <amrollahi.daneshvar@gmail.com>
2022-08-18 15:55:45 +0000

    Added doc and test for memcmp

--- examples/memcmp/memcmp.c
+++ examples/memcmp/memcmp.c
@@ -1,6 +1,10 @@
 #include "klee/klee.h"
 #include <stdio.h>
 
+/*
+match(char *a, char* b, int n) returns 0 if a and b are equal, and 1 otherwise.
+*/
+
 int match(char *a, char* b, int n) 
 {
         klee_assert(!klee_is_symbolic(&n));
@@ -19,12 +23,18 @@ int main() {
         int res = match(a, b, n);
         if (res == 1)
         {
+                /* klee_assume(a[0] == b[0]);
+                klee_assume(a[1] == b[1]);
+                klee_assume(a[2] == b[2]);
+                klee_assume(a[3] == b[3]);
+                klee_assume(a[4] == b[4]); // must fail on this */
                 x = 2;
                 printf("match = 1\n");
         }
         else
         {
                 x = 3;
+                /* klee_assume(a[0] != b[0]); // must fail on this */
                 printf("match != 1\n");
         }
         
