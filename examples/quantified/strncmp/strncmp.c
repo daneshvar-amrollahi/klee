08b8a124d49560ddeee20ba2f9b004fb57d85c3f
Daneshvar Amrollahi <amrollahi.daneshvar@gmail.com>
2022-09-08 14:52:59 +0000

    Summarized and tested strncmp

--- examples/quantified/strncmp/strncmp.c
+++ examples/quantified/strncmp/strncmp.c
@@ -4,34 +4,28 @@
 
 int match(char *a, char* b, int n) 
 {
-        klee_assert(!klee_is_symbolic(&n));
-        uint32_t i;
-        klee_make_symbolic(&i, sizeof(i), "strncmp_return_value");
-        klee_strncmp(a, b, n, i);
-        return i;
+        uint32_t ret;
+        klee_make_symbolic(&ret, sizeof(ret), "strncmp_return_value");
+        klee_strncmp(a, b, n, ret);
+        return ret;
 }
 
 int main() {
         char a[5], b[5];
         klee_make_symbolic(a, sizeof(a), "a");
         klee_make_symbolic(b, sizeof(b), "b");
-        int n = 5, x;
-        int res = match(a, b, n);
+        int n = 5, res = match(a, b, n);
 
-        if (res == 0)
-        {
-                // klee_assume(a[3] != b[3]); //should fail on this
-                x = 2;
-                printf("equal\n");
-        }
-        else
-        {
-                // klee_assume(a[0] == b[0]);
-                // klee_assume(a[1] == b[1]);
-                // klee_assume(a[2] == b[2]);
-                // klee_assume(a[3] == b[3]);
-                // klee_assume(a[4] == b[4]);
-                x = 3;
-                printf("not equal\n");
-        }
+        // klee_assume(res == 0);
+        // klee_assume(a[2] != b[2]);
+
+        // klee_assume(res < 0);
+        // klee_assume(a[0] == b[0]);
+        // klee_assume(a[1] == b[1]);
+        // klee_assume(a[2] == b[2]);
+        // klee_assume(a[3] > b[3]);
+
+        // klee_assume(res > 0);
+        // klee_assume(a[0] == b[0]);
+        // klee_assume(a[0] < b[0]);
 }
